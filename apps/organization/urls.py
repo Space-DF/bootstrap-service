dd0ee8df920358093df20952110882d661faf720
ngovinh2k2 <vinh.ngo@digitalfortress.dev>
2026-01-16 14:22:45 +0700

    feat: check slug org api

--- apps/organization/urls.py
+++ apps/organization/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
-from apps.organization.views import OrganizationView
+from apps.organization.views import CheckOrganizationView, OrganizationView
 
 app_name = "organization"
 
 urlpatterns = [
     path("organizations", OrganizationView.as_view(), name="organization"),
+    path(
+        "organizations/check/<str:slug_name>",
+        CheckOrganizationView.as_view(),
+        name="check-organization",
+    ),
 ]
--- apps/organization/views.py
+++ apps/organization/views.py
@@ -1,7 +1,9 @@
 from common.pagination.base_pagination import BasePagination
 from django.db.models import CharField, Count, Subquery
 from django.shortcuts import get_object_or_404
+from rest_framework import status, views
 from rest_framework.filters import OrderingFilter, SearchFilter
+from rest_framework.response import Response
 
 from apps.organization.models import Organization
 from apps.organization.serializers import OrganizationSerializer
@@ -43,3 +45,22 @@ class OrganizationView(OrganizationRetrieveAPIView):
             )
             .distinct()
         )
+
+
+class CheckOrganizationView(views.APIView):
+    authentication_classes = []
+
+    def get(self, request, slug_name):
+        organization = Organization.objects.filter(slug_name=slug_name).first()
+        if not organization:
+            return Response(
+                {"result": f"Organization with slug '{slug_name}' not found."},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+        if not organization.is_active:
+            result = "The organization is deactivated!"
+        else:
+            result = "The organization is valid."
+
+        return Response({"result": result}, status=status.HTTP_200_OK)
